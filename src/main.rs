9c922070e84e43f12403f226650563b1d36f8c17
dshargool <dshargool2009@gmail.com>
2023-04-24 22:01:26 -0600

    Basic routing skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
+use axum::{response::Html, routing::get, Router, extract::Path};
+
+async fn root() -> String {
+    format!("Hiya Guy")
+}
+
+async fn greet(Path(user_name): Path<String>) -> String {
+    format!("Hello {}", &user_name)
+}
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+
+    let app = Router::new().route("/", get(root))
+        .route("/*user_name", get(greet));
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    .serve(app.into_make_service())
+    .await
+    .unwrap();
 }
